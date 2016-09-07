15699f379591b17a34673d413b3f39cd08c2b50c
Gegene79 <fabienvignat@yahoo.fr>
2016-09-08 00:48:35 +0200

    nueva versión de la api que devuelve los valores haciendo medias sobre intervalos de 5 minutos

--- routes/api.js
+++ routes/api.js
@@ -21,83 +21,163 @@ router.use(function (req, res, next) {
 
 
 router.get('/', function(req, res, next) {
-    db.Metric.find({
-        timestamp: {
-        $gte: ini,
-        $lte: end
+    db.Metric.aggregate(
+        [
+         { $match : {
+                    timestamp: {$gte: new Date(ini), $lte: new Date(end)} 
+                }
+        },   
+        { "$group": {
+                "_id": {
+                    timestamp: {
+                        "$add": [
+                            { "$subtract": [
+                                { "$subtract": [ "$timestamp", new Date(0) ] },
+                                { "$mod": [ 
+                                    { "$subtract": [ "$timestamp", new Date(0) ] },
+                                        1000 * 60 * 5
+                                    ]}
+                                ]},
+                            new Date(0)
+                            ]
+                        },
+                        name:"$name", type:"$type"
+                },
+                //"count": { "$sum": 1 },
+                //"first": { "$first": "$value"},
+                //"last": { "$last": "$value"},
+                "avg": { "$avg": "$value"}//,
+                //"min": { "$min": "$value"},
+                //"max": { "$max": "$value"},
+                //"stdev": { "$stdDevPop": "$value"}
+            }
         }
-    },'-_id type name timestamp value')
-    .sort('type')
-    .sort('name')
-    .sort('timestamp')
-    .exec(function (err, docs) {
+    ],
+    function (err, docs) {
         res.json(docs);
     });
 });
 
-/* TODO : terminar esa query
+// get current values
 router.get('/current', function(req, res, next) {
     
     db.Metric.aggregate(
         [
-        { $sort: {type: 1, name: 1, timestamp:1} },
-        { $group: {_id: {"$name","$type"}, timestamp: {$last: "$timestamp" }}}
+        { $group: {_id: {name:"$name", type:"$type"}, timestamp: {$last: "$timestamp" }, value: { $last: '$value'}}},
+        { $sort: {_id: 1}}
         ], 
         function (err, docs) {
             res.json(docs);
     });
 });
-*/
 
 router.get('/:type', function(req, res, next) {
-  db.Metric.find({
-        type: req.params.type,
-        timestamp: {
-        $gte: ini,
-        $lte: end
+    db.Metric.aggregate(
+        [
+         { $match : {
+                    type: req.params.type,
+                    timestamp: {$gte: new Date(ini), $lte: new Date(end)} 
+                }
+        },   
+        { "$group": {
+                "_id": {
+                    timestamp: {
+                        "$add": [
+                            { "$subtract": [
+                                { "$subtract": [ "$timestamp", new Date(0) ] },
+                                { "$mod": [ 
+                                    { "$subtract": [ "$timestamp", new Date(0) ] },
+                                        1000 * 60 * 5
+                                    ]}
+                                ]},
+                            new Date(0)
+                            ]
+                        },
+                        name:"$name"
+                },
+                //"count": { "$sum": 1 },
+                //"first": { "$first": "$value"},
+                //"last": { "$last": "$value"},
+                "avg": { "$avg": "$value"}//,
+                //"min": { "$min": "$value"},
+                //"max": { "$max": "$value"},
+                //"stdev": { "$stdDevPop": "$value"}
+            }
         }
-    },'-_id name timestamp value')
-    .sort('name')
-    .sort('timestamp')
-    .exec(function (err, docs) {
+    ],
+    function (err, docs) {
         res.json(docs);
-    });});
+    });  
 
-router.get('/:type/:name', function(req, res, next) {
-
-  if(req.params.type && req.params.name){
-    db.Metric.find({ 
-      name: req.params.name,
-      type: req.params.type,
-      timestamp: {
-        $gte: ini,
-        $lte: end
-        }
-   },'-_id timestamp value')
-   .sort('timestamp')
-   .exec(function (err, docs) {
-            res.json(docs);
-    });
-  } else {
-      res.render('index', { title: req.params.name });
-  }
 });
 
-// get most up-to-date values
-router.get('/:type/:name/current', function(req, res, next) {
-
-  if(req.params.type && req.params.name){
-    db.Metric.findOne({ 
-      name: req.params.name,
-      type: req.params.type,
-   },'-_id timestamp value')
-   .sort('-timestamp')
-   .exec(function (err, docs) {
+// get current values of some type
+router.get('/:type/current', function(req, res, next) {
+    
+    db.Metric.aggregate(
+        [
+        { $match: {type: req.params.type}},
+        { $group: {_id: "$name", timestamp: {$last: "$timestamp" }, value: { $last: '$value'}}},
+        { $sort: {_id: 1}}
+        ], 
+        function (err, docs) {
             res.json(docs);
     });
-  } else {
-      res.render('index', { title: req.params.name });
-  }
+});
+
+
+
+router.get('/:type/:name', function(req, res, next) {
+    db.Metric.aggregate(
+        [
+         { $match : {
+                    name: req.params.name,
+                    type: req.params.type,
+                    timestamp: {$gte: new Date(ini), $lte: new Date(end)} 
+                }
+        },   
+        { "$group": {
+                "_id": {
+                    timestamp: {
+                        "$add": [
+                            { "$subtract": [
+                                { "$subtract": [ "$timestamp", new Date(0) ] },
+                                { "$mod": [ 
+                                    { "$subtract": [ "$timestamp", new Date(0) ] },
+                                        1000 * 60 * 5
+                                    ]}
+                                ]},
+                            new Date(0)
+                            ]
+                        }
+                },
+                //"count": { "$sum": 1 },
+                //"first": { "$first": "$value"},
+                //"last": { "$last": "$value"},
+                "avg": { "$avg": "$value"}//,
+                //"min": { "$min": "$value"},
+                //"max": { "$max": "$value"},
+                //"stdev": { "$stdDevPop": "$value"}
+            }
+        }
+    ],
+    function (err, docs) {
+        res.json(docs);
+    });
+});
+
+// get most up-to-date value for a type and a name
+router.get('/:type/:name/current', function(req, res, next) {
+
+  db.Metric.aggregate(
+        [
+        { $match : { name: req.params.name, type: req.params.type } },
+        { $group: {_id: "", timestamp: {$last: "$timestamp" }, value: { $last: '$value'}}},
+        { $sort: {_id: 1}}
+        ], 
+        function (err, docs) {
+            res.json(docs);
+    });
 });
 
 // insert metric
