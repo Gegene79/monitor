5f039560f9d813939e02a404d1200358a72e8357
Gegene79 <fabienvignat@yahoo.fr>
2016-12-10 21:21:54 +0100

    sorted before limiting the query scan more robust and quicker corrected of new date lines

--- common/fsdbsync.js
+++ common/fsdbsync.js
@@ -18,7 +18,7 @@ const factor = cst.CPU_SCAN_FACTOR;
 
 function logmem(str){
     memory = process.memoryUsage();
-    console.log(str+'\nTotal: '+(memory.heapTotal/(1024*1024)).toFixed(2)+'\nUsed: '+(memory.heapUsed/(1024*1024)).toFixed(2));
+    console.log(str+'Total: '+(memory.heapTotal/(1024*1024)).toFixed(2)+' MB, Used: '+(memory.heapUsed/(1024*1024)).toFixed(2)+' MB');
 };
 
 function getthumbpath(imagepath,thumbprefix){
@@ -42,7 +42,7 @@ function getsibling(thumbpath,sourceprefix,targetprefix){
 
 function createthumb(image,thumbheight,thumbprefix)  {
     
-    logmem('debut '+image);
+    logmem('');
 
     return new Promise(function(resolve, reject){
 
@@ -52,8 +52,8 @@ function createthumb(image,thumbheight,thumbprefix)  {
         // obtain the size of an image
         imageMagick(image).size(function (err, size) {
             if (err) {
-                console.log(err);
-                return reject(err);
+                console.error('error while getting image size '+err);
+                return resolve();
             } else {
                 //console.log(new Date().toISOString()+ ' - '+image+' size=' + size.width+'x'+size.height);
 
@@ -66,11 +66,10 @@ function createthumb(image,thumbheight,thumbprefix)  {
                     .autoOrient()
                     .write(thumb, function (err) {
                         if (err) {
-                            console.log(err);
-                            return reject(err);
+                            console.error('error while writing resized image: '+err);
+                            return resolve();
                         } else {
                             console.log('created thumb '+thumb);
-                            logmem('fin '+image);
                             return resolve(thumb);
                         }
                     });
@@ -80,7 +79,8 @@ function createthumb(image,thumbheight,thumbprefix)  {
                         console.log('copied image '+ image+' as thumb '+thumb);
                         return resolve(thumb);
                     }).catch(function(error){
-                        return reject(error);
+                        console.error('error while copying image '+image+', error: '+error);
+                        return resolve();
                     });
                 }
             }
@@ -97,31 +97,29 @@ function deletefile(thumb){
             return Promise.resolve(result);
         })
         .catch(function(error){
-            return Promise.reject(new Error("error intentando borrar fichero "+thumb));
+            console.error("error intentando borrar fichero "+thumb);
+            return Promise.resolve();
         });
     } else {
-        return Promise.reject(new Error("solo se admite borrar en thumbs. aqui: "+thumb));
+        console.error("solo se admite borrar en thumbs. aqui: "+thumb);
+        return Promise.resolve();
     }
 }
 
 
 function getexif(path){
-    logmem('exif debut '+path);
     return new Promise(function(resolve, reject) {
         try {
             new ExifImage({ image : path }, function (error, exifData) {
                 if (error){
-                    console.log('Error in getexif for '+path+': '+ error.message);
-                    logmem('exif fin '+path);
+                    console.error('Error in getexif for '+path+': '+ error.message);
                     return resolve(null);
                 } else {
-                    logmem('exif fin '+path);
                     return resolve(exifData);
                 }
             });
         } catch (error) {
-            console.log('getexif error for '+path+': '+ error.message);
-            logmem('exif fin '+path);
+            console.error('getexif error for '+path+': '+ error);
             return resolve(null);
         }
     });
@@ -129,34 +127,32 @@ function getexif(path){
 
 function enrichimage(imgdetails){
     
-    if (!(imgdetails) || imgdetails.length==0) {return Promise.resolve(null);}
+    if (!(imgdetails) || imgdetails.length==0) {return Promise.resolve();}
 
     let imgpath = imgdetails[0];
     let stats = imgdetails[1];
     let exifdata = imgdetails[2];
 
-    logmem('enrich debut '+imgpath);
-    
     if (!(stats.isFile())){ 
-        console.log(imgpath + " is not a file");
-        return Promise.resolve(null);
+        console.info(imgpath + " is not a file");
+        return Promise.resolve();
     }
-    // moment(exifdata.exif.CreateDate,['YYYY:MM:DD HH:mm:ss','YYYY/MM/DD HH:mm:ss');
     let thumb = getthumbpath(imgpath,THUMBS_L_PREFIX);
     let sthumb = getsibling(thumb,THUMBS_L_PREFIX,THUMBS_S_PREFIX);
     let created_at = undefined;
-
+    let reldir = path.dirname(imgpath).substring(cst.IMAGES_DIR.length);
     var image = {
             path : imgpath,
             filename : path.basename(imgpath),
             dir : path.dirname(imgpath),
-            rel_dir: path.dirname(imgpath).substring(cst.IMAGES_DIR.length),
+            rel_dir: reldir,
             extension : path.extname(imgpath),
             largethumb: thumb,
             smallthumb: sthumb,
             mtime : stats.mtime,
             ctime : stats.ctime,
-            birthtime : stats.birthtime
+            birthtime : stats.birthtime,
+            size : stats.size
     };
     
     if (exifdata!=null){
@@ -197,8 +193,6 @@ function enrichimage(imgdetails){
     let strctime = m.format('DD MMMM YYYY');
     image.str_created_at = strctime;
 
-    logmem('enrich fin '+imgpath);
-    
     return Promise.resolve(image);
 }
 
@@ -211,8 +205,8 @@ function insertimage(path){
         return db.insertImage(image);
     })
     .catch(function (error){
-        console.log('error en tratar imagen: '+error);
-        return Promise.reject(error);
+        console.error('error en tratar imagen: '+error);
+        return Promise.resolve();
     });
 }
 
@@ -221,61 +215,66 @@ function scan() {
     var imagespattern = cst.IMAGES_DIR+"/**/*.jpg";
     var thumbspattern = cst.THUMBS_DIR+"/**/"+THUMBS_L_PREFIX+"*.jpg";
 
-    console.log("*** Starting scan. ***");
+    logmem("*** Starting scan. ***\n");
 
     Promise.all([glob.globAsync(imagespattern,{nocase:true}),
                  db.listAllImages(), 
                  glob.globAsync(thumbspattern,{nocase:true})])
     .then(function(results) {
-        let fsimages = results[0];
-        let dbimages = results[1];
+        var fsimages = results[0];
+        var dbimages = results[1];
         let fsthumbs = results[2];
+        logmem("Got results from db and thumb fs and image fs\n");
 
+        var m_dbimg = new Map();
+        dbimages.forEach(function(element){
+            m_dbimg.set(element.path,element._id);
+        });
+
+        var m_thumbs = new Map();
+        fsthumbs.forEach(function(element){
+            m_thumbs.set(getimagepath(element,THUMBS_L_PREFIX));
+        });
+
+        let m_img = new Map();
+        fsimages.forEach(function(element){
+            m_img.set(element);
+        });
+
+        logmem('Maps loaded\n');
+        return Promise.resolve([m_dbimg,m_thumbs,m_img]);
+    }).then(function(r){
+        let m_dbimg = r[0];
+        let m_thumbs = r[1];
+        let m_img = r[2];
         // check images found on fs versus in DB
         let imagestoinsert = [];
         let thumbstocreate = []; 
-        fsimages.forEach(function(item){
-            
-            let im = dbimages.findIndex(function(element,index,array){return element.path==item});
+        m_img.forEach(function(val,item,m){
 
-            if (im < 0){
-                //console.log("file not found in db: "+ item);
-                //insertimage(item);
+            if (!m_dbimg.has(item)){
                 imagestoinsert.push(item);
             }
 
-            let thb = fsthumbs.findIndex(function(element,index,array){
-                let imgpath = getimagepath(element,THUMBS_L_PREFIX);
-                return ( imgpath == item);
-            });
-
-            if (thb < 0){
-                //console.log("file not found in thumbs: "+ item);
+            if (!m_thumbs.has(item)){
                 thumbstocreate.push(item);
             }
         });
         
         // check images found in DB versus on fs and delete the obsolete ones
         let imagestodelete = [];
-        dbimages.forEach(function (item){
+        m_dbimg.forEach(function (val,item,m){
             
-            let im = fsimages.findIndex(function(element,index,array){return element==item.path});
-
-            if (im < 0){ // esta en BBDD pero no en el fs, remove from db
-                imagestodelete.push(item._id);
+            if (!m_img.has(item)){ // esta en BBDD pero no en el fs, remove from db
+                imagestodelete.push(val);
             }
         });
         
         let thumbstodelete = [];
-        fsthumbs.forEach(function(item){
+        m_thumbs.forEach(function(val,item,m){
 
-            let thb = fsimages.findIndex(function(element,index,array){
-                let thbpath = getthumbpath(element,THUMBS_L_PREFIX);
-                return ( thbpath == item);
-            });
-
-            if (thb < 0){ // esta en los thumbnails pero no en las imagenes
-                thumbstodelete.push(item);
+            if (!m_img.has(item)){ // esta en los thumbnails pero no en las imagenes
+                thumbstodelete.push(getthumbpath(item,THUMBS_L_PREFIX));
             }
         });
 
@@ -283,8 +282,9 @@ function scan() {
         Images to insert: "+imagestoinsert.length+",\n \
         Thumbs to create: "+thumbstocreate.length+",\n \
         Images to delete: "+imagestodelete.length+",\n \
-        Thumbs to delete: "+thumbstodelete.length+".\n");
-
+        Thumbs to delete: "+thumbstodelete.length+".");
+        logmem('');
+        
         Promise.map(imagestoinsert,function(image){
             return insertimage(image);
         }, {concurrency: cst.CPUNB*cst.CPU_SCAN_FACTOR})
@@ -316,7 +316,6 @@ function scan() {
         .catch(function(error){
             console.log("Error: "+error);
         });
-
     });
 }
 
